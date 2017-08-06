bac6359a4312c789e238c7007ff693a94a3d0b65
probonopd <probonopd@users.noreply.github.com>
2017-08-06 19:07:25 +0200

    Update travis.sh

--- code/travis.sh
+++ code/travis.sh
@@ -6,5 +6,5 @@ FILES=$(git log -p -1 data/ | grep +++ | cut -d '/' -f 2-)
 # Work on thes files that have been changed in the last commit
 for FILE in $FILES ; do
   echo "$FILE"
-  cat "$FILE"
+  code/worker.py "$FILE"
 done
