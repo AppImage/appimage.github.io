bbb17c47c2c67663a7a6e37daca7e301a3954229
probonopd <probonopd@users.noreply.github.com>
2017-08-06 23:16:42 +0200

    Update worker.py

--- code/worker.py
+++ code/worker.py
@@ -13,6 +13,7 @@ if not firstline.startswith("http"):
     exit(1)
 
 # Download the AppImage
+# TODO: Optionally FUSE-mount the AppImage with httpfs; is it really worth it?
 filename = firstline.split('/')[-1].split('#')[0].split('?')[0]
 urllib.request.urlretrieve(firstline, filename)
 
@@ -21,4 +22,17 @@ f = open(filename)
 f.seek(8)
 data = f.read(3)
 for c in data:
-    print(c)
+    print(c) # FIXME; should be 0x414902
+
+# Exit if the magic bytes are not 0x414901 (type 1) or 0x414902 (type 2)
+
+# If 0x414902 then extract desktop file like so:
+# export TARGET_APPIMAGE = filename
+# ./runtime --appimage-extract '*.desktop'
+# ...
+
+# If 0x414901 then loop-mount with fuseiso
+# ...
+
+# Run some checks
+# Export data from the desktop file to "database file"
