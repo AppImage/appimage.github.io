aaf33fe1a5f3c7a9446c6025b502921f45e5fa30
Zygmunt Krynicki <me@zygoon.pl>
2017-08-07 00:32:49 +0200

    Fix some flake8 warnings
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- code/worker.py
+++ code/worker.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
+"""AppImage hub worker scrpit."""
 import argparse
 
 import requests
 
 
 def main() -> None:
+    """Process worker request as given on command line."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'appimage_url', metavar='URL',
         help="URL of the AppImage file to query")
     ns = parser.parse_args()
-    r = requests.get(ns.appimage_url, stream=True)
-    r.raise_for_status()
-    header = r.raw.read(8+3)
+    req = requests.get(ns.appimage_url, stream=True)
+    req.raise_for_status()
+    header = req.raw.read(8+3)
     tag = header[8:8+3]
-    name = r.url
+    name = req.url
     if tag == b'AI\x01':
         print("{} looks like a valid type-1 AppImage file".format(name))
     elif tag == b'AI\x02':
         print("{} looks like a valid type-2 AppImage file".format(name))
     else:
-        raise SystemExit("{} doesn't look like a valid AppImage file".format(name))
+        raise SystemExit(
+                "{} doesn't look like a valid AppImage file".format(name))
     # If 0x414902 then extract desktop file like so:
     # export TARGET_APPIMAGE = filename
     # ./runtime --appimage-extract '*.desktop'
@@ -33,8 +36,5 @@ def main() -> None:
     # Export data from the desktop file to "database file"
 
 
-
 if __name__ == "__main__":
     main()
-
-
