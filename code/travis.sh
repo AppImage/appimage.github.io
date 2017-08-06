1d81a977745ceb5553d9ba858fab64b75d39ce7a
probonopd <probonopd@users.noreply.github.com>
2017-08-06 18:41:43 +0200

    Update travis.sh

--- code/travis.sh
+++ code/travis.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "This is a placeholder"
-git log -p -10 data/
+
+# Find out which files in data/ have been changed in the last commit
+FILES=$(git log -p -1 data/ | grep +++ | cut / -f 2-)
+
+# Work on thes files that have been changed in the last commit
+for FILE in $FILES ; do
+  echo "$FILE"
+  cat "$FILE"
+done
