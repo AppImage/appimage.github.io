e4f15ca2348eb8e5f819112f4260d04f2260650e
probonopd <probonopd@users.noreply.github.com>
2017-08-06 21:46:54 +0200

    Update worker.py

--- code/worker.py
+++ code/worker.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 
 import os, sys
+import urllib.request
 
+# Get the URL to the AppImage
 f = open(sys.argv[1], "r") 
-print(f.readline())
+firstline = f.readline()
+f.close()
+print(firstline)
+if not firstline.startswith("http"):
+  print("%s seems not to contain an URL, exiting" % (sys.argv[1]))
+  exit(1)
+
+# Download the AppImage
+filename = firstline.split('/')[-1].split('#')[0].split('?')[0]
+urllib.request.urlretrieve(firstline, filename)
+
+# Get the magic bytes
+f = open(filename)
+f.seek(8)
+print(hex(f.read(1)))
+print(hex(f.read(1)))
+print(hex(f.read(1)))
+f.seek(8)
+print(hex(f.read(3)))
 f.close()
