63fa583fc3bd147dd23d56a99c058262fefdb131
probonopd <probonopd@users.noreply.github.com>
2017-08-08 16:46:04 +0200

    Update worker.sh

--- code/worker.sh
+++ code/worker.sh
@@ -87,8 +87,11 @@ echo "==========================================="
 # and take screenshots if we don't have them already from AppStream
 
 # LD_DEBUG=libs "$APPDIR/AppRun" & # Getting "Desktop file is missing. Please run /mnt/AppRun from within an AppImage." with wire-2.15.2751-x86_64.AppImage
-chmod +x "$FILENAME"
-firejail --x11=xpra ./"$FILENAME" &
+# chmod +x "$FILENAME"
+wet -c https://sourceforge.net/projects/firejail/files/firejail/firejail_0.9_1_amd64.deb/download -O firejail_0.9_1_amd64.deb
+sudo dpkg -i firejail_0.9_1_amd64.deb
+
+firejail --x11=xpra --appimage ./"$FILENAME" &
 APID=$!
 sleep 5
 kill $APID && echo "SUCCESS" || exit 1
