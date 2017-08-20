5c4e9699843be0fde839c5c85a07fbc632e0f2e4
probonopd <probonopd@users.noreply.github.com>
2017-08-20 13:52:46 +0200

    Try to run in xterm

--- code/worker.sh
+++ code/worker.sh
@@ -139,57 +139,57 @@ echo "==========================================="
 echo "============= TRYING TO RUN ==============="
 echo "==========================================="
 
-reset
-firejail --noprofile --net=none --appimage ./"$FILENAME" &
+# reset does not work here
+if [ "$TERMINAL" == "false" ] ; then
+  firejail --noprofile --net=none --appimage ./"$FILENAME" &
+else
+  xterm -hold -e firejail --noprofile --net=none --appimage ./"$FILENAME" &
+fi
 APID=$!
 sleep 10
 
 # Make a screenshot
-if [ "$TERMINAL" == "false" ] ; then
 
+# Get a list of open windows
+xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//'
+
+# Count the windows on screen
+NUMBER_OF_WINDOWS=$(xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//' | wc -l)
+echo "NUMBER_OF_WINDOWS: $NUMBER_OF_WINDOWS"
+if [ $(($NUMBER_OF_WINDOWS)) -lt 1 ] ; then
+  echo "ERROR: Could not find a single window on screen :-("
+fi
+
+# Works with Xvfb but cannot select window by ID
+# sudo apt-get -y install scrot 
+# scrot -b 'screenshot_$wx$h.jpg' # -u gives "X Error of failed request:  BadDrawable (invalid Pixmap or Window parameter)"
+# mv screenshot_* database/$INPUTBASENAME/
+
+# Getting the active window seems to require a window manager
+icewm &
+sleep 2
+
+# We could simulate X11 keyboard/mouse input with xdotool here if needed;
+# of course this should not be hardcoded here (this is just an example)
+if [ "$INPUTBASENAME" == "VLC" ] ; then
+  xdotool sleep 0.1 key Return # Click away the data protection window
+  xdotool sleep 0.1 key shift+F1 # Open the about screen
+  sleep 1
+fi
+if [ "$INPUTBASENAME" == "Subsurface" ] ; then
+  xdotool sleep 0.1 key Escape # Click away the update check window
+  sleep 1
   # Get a list of open windows
   xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//'
-  
-  # Count the windows on screen
-  NUMBER_OF_WINDOWS=$(xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//' | wc -l)
-  echo "NUMBER_OF_WINDOWS: $NUMBER_OF_WINDOWS"
-  if [ $(($NUMBER_OF_WINDOWS)) -lt 1 ] ; then
-    echo "ERROR: Could not find a single window on screen :-("
-  fi
-  
-  # Works with Xvfb but cannot select window by ID
-  # sudo apt-get -y install scrot 
-  # scrot -b 'screenshot_$wx$h.jpg' # -u gives "X Error of failed request:  BadDrawable (invalid Pixmap or Window parameter)"
-  # mv screenshot_* database/$INPUTBASENAME/
-  
-  # Getting the active window seems to require a window manager
-  icewm &
-  sleep 2
-  
-  # We could simulate X11 keyboard/mouse input with xdotool here if needed;
-  # of course this should not be hardcoded here (this is just an example)
-  if [ "$INPUTBASENAME" == "VLC" ] ; then
-    xdotool sleep 0.1 key Return # Click away the data protection window
-    xdotool sleep 0.1 key shift+F1 # Open the about screen
-    sleep 1
-  fi
-  if [ "$INPUTBASENAME" == "Subsurface" ] ; then
-    xdotool sleep 0.1 key Escape # Click away the update check window
-    sleep 1
-    # Get a list of open windows
-    xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//'
-  fi
-  
-  # Works with Xvfb
-  # sudo apt-get -y install x11-apps netpbm xdotool # We do this in .travis.yml
-  # -display :99 needed here? 
-  # xwd -id $(xdotool getactivewindow) -silent | xwdtopnm | pnmtojpeg  > database/$INPUTBASENAME/screenshot.jpg && echo "Snap!"
-  mkdir -p database/$INPUTBASENAME/
-  # xwd -id $(xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//' | head -n 1 | cut -d " " -f 1) -silent | xwdtopnm | pnmtojpeg  > database/$INPUTBASENAME/screenshot.jpg && echo "Snap!"
-  xwd -id $(xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//' | head -n 1 | cut -d " " -f 1) -silent | xwdtopnm | pnmtopng  > database/$INPUTBASENAME/screenshot.png && echo "Snap!"
-else
-  fbgrab database/$INPUTBASENAME/screenshot.png && echo "cliSnap!"
 fi
+
+# Works with Xvfb
+# sudo apt-get -y install x11-apps netpbm xdotool # We do this in .travis.yml
+# -display :99 needed here? 
+# xwd -id $(xdotool getactivewindow) -silent | xwdtopnm | pnmtojpeg  > database/$INPUTBASENAME/screenshot.jpg && echo "Snap!"
+mkdir -p database/$INPUTBASENAME/
+# xwd -id $(xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//' | head -n 1 | cut -d " " -f 1) -silent | xwdtopnm | pnmtojpeg  > database/$INPUTBASENAME/screenshot.jpg && echo "Snap!"
+xwd -id $(xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//' | head -n 1 | cut -d " " -f 1) -silent | xwdtopnm | pnmtopng  > database/$INPUTBASENAME/screenshot.png && echo "Snap!"
 
 kill $APID && printf "\n\n\n* * * SUCCESS :-) * * *\n\n\n" || exit 1
 killall icewm
