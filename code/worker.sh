68749579ba1117ba4463e17e07c6fd6337d6f509
probonopd <probonopd@users.noreply.github.com>
2023-08-27 13:30:04 +0200

    Use imgur
    [ci skip]

--- code/worker.sh
+++ code/worker.sh
@@ -564,7 +564,8 @@ if [ "$IS_PULLREQUEST" = true ]; then
   cat "apps/${INPUTBASENAME}.md" || exit 1
   cat "database/${INPUTBASENAME}/"*.desktop || exit 1 # Asterisk must not be inside quotes, https://travis-ci.org/AppImage/appimage.github.io/builds/360847207#L782
   ls -lh "database/${INPUTBASENAME}/screenshot.png" || exit 1
-  curl --upload-file "database/${INPUTBASENAME}/screenshot.png" https://transfersh.com/screenshot.png
+  wget -q https://raw.githubusercontent.com/tremby/imgur.sh/1c64feeefb6590741eb3d034575f9c788469b0a8/imgur.sh
+  bash imgur.sh "database/${INPUTBASENAME}/screenshot.png"
   echo ""
   echo "We will assume the test is OK (a pull request event was triggered and the required files exist)."
   exit 0
