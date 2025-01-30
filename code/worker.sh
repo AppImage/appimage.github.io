b2b24a4166b4c502397c5d04b02d388e080fc3ec
Sebastian Stenzel <overheadhunter@users.noreply.github.com>
2025-01-30 20:58:33 +0100

    clean database directory before recreating metadata (#3439)
    * fix #3438
    
    cleaning any previously created files when re-running the script
    
    * apply suggestions
    
    and make sure to delete the whole folder before recreating it

--- code/worker.sh
+++ code/worker.sh
@@ -294,6 +294,11 @@ if [ x"$INPUTBASENAME" == xSubsurface ] ; then
   xwininfo -tree -root | grep 0x | grep '": ("' | sed -e 's/^[[:space:]]*//'
 fi
 
+# Clean residue from previous runs, avoiding issue #3438
+if [ -n "$INPUTBASENAME" ] && [ -d "database/$INPUTBASENAME" ]; then
+    rm -r "database/$INPUTBASENAME"
+fi
+
 # Works with Xvfb
 # sudo apt-get -y install x11-apps netpbm xdotool # We do this in .travis.yml
 # -display :99 needed here?
